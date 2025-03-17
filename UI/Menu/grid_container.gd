886e1ecb75e4d7ad2f981ff82e1b86dd3f248c24
sushimanz <58267079+sushimanz@users.noreply.github.com>
2025-03-16 21:54:07 -0400

    First Commit
    initial commit

---
+++ UI/Menu/grid_container.gd
@@ -0,0 +1,11 @@
+extends GridContainer
+
+
+
+
+func _on_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_mouse_exited() -> void:
+	pass # Replace with function body.
---
+++ UI/Menu/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/Menu/choose_your_character.tscn")
+
+
+func _on_settings_button_pressed() -> void:
+	print("settings button pressed")  
+	#get_tree().change_scene_to_file()
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
