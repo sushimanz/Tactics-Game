378da4da4247cea4b3f35780a7042b722b7d19c2
RandomGuyOnTheNet007 <167091842+RandomGuyOnTheNet007@users.noreply.github.com>
2025-03-23 01:12:27 +0530

    Button hover and press tweens
    Just some simple tweening to buttons. I also changed the button to a texture button and added a texture to it

--- UI/Menu/main_menu.gd
+++ UI/Menu/main_menu.gd
@@ -4,7 +4,16 @@ signal noray_connected
 
 @onready var multi_id 
 @onready var oid_lbl = $OIDText
-@onready var oid_input = $JoinButton/LineEdit
+@onready var oid_input = $LineEdit
+
+@onready var buttons := [
+	$Buttons/StartButton,
+	$Buttons/HostButton,
+	$Buttons/JoinButton,
+	$Buttons/SettingsButton,
+	$Buttons/ExitButton,
+	$Buttons/CopyButton
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,22 +21,56 @@ func _ready() -> void:
 	await  Multiplayer.noray_connected
 	oid_lbl.text = Noray.oid
 
+	for button in buttons: # simple loop to bind the buttons to the functions
+		button.connect("mouse_entered", Callable(self, "_on_mouse_enter").bind(button))
+		button.connect("mouse_exited", Callable(self, "_on_mouse_exit").bind(button))
+		button.connect("pressed", Callable(self, "_on_mouse_press").bind(button))
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
+func _on_mouse_enter(button):
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(button, "scale", Vector2(1.2, 1.2), 0.5)
+	
+func _on_mouse_exit(button):
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(button, "scale", Vector2(1.0, 1.0), 0.5)
+	
+func _on_mouse_press(button):
+	var tween = get_tree().create_tween()
+	tween.tween_property(button, "scale", Vector2(0.9, 0.9), 0.1).set_ease(Tween.EASE_OUT)
+	tween.tween_property(button, "rotation_degrees", -5, 0.1)
+	tween.parallel().tween_property(button, "modulate", Color(0.8, 0.8, 0.8), 0.1)
+	tween.tween_property(button, "scale", Vector2(1.0, 1.0), 0.2).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(button, "rotation_degrees", 0, 0.2)
+	tween.parallel().tween_property(button, "modulate", Color(1, 1, 1), 0.2)
+	
 func _on_start_button_pressed() -> void:
+	await get_tree().create_timer(0.4).timeout
+	
 	get_tree().change_scene_to_file("res://Game/Game.tscn")
 
 
+# Added timeout in all the buttons else the animation won't even display
+
 func _on_settings_button_pressed() -> void:
+	await get_tree().create_timer(0.4).timeout
+	
 	print("settings button pressed")  
 	#get_tree().change_scene_to_file()
 
 
 func _on_exit_button_pressed() -> void:
+	await get_tree().create_timer(0.4).timeout
+
+	
 	get_tree().quit()
 
 
@@ -36,11 +79,15 @@ func _on_copy_button_pressed() -> void:
 
 
 func _on_join_button_pressed() -> void:
+	await get_tree().create_timer(0.4).timeout
+	
 	Multiplayer.join(oid_input.text)
 	get_tree().change_scene_to_file("res://Game/Game.tscn")
 
 
 func _on_host_button_pressed() -> void:
+	await get_tree().create_timer(0.4).timeout
+	
 	Multiplayer.host()
 	
 	get_tree().change_scene_to_file("res://Game/Game.tscn")
