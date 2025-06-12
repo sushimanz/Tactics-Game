684d29af4dd65ddee18e103939f313a470ce2b92
sushimanz <58267079+sushimanz@users.noreply.github.com>
2025-06-11 22:17:14 -0400

    2d array

--- Game/grid.gd
+++ Game/grid.gd
@@ -1,12 +1,15 @@
 extends Control
 
 var grid = ResData.grid
-var gridData: Dictionary[Tile, Unit]
+#var gridData: Dictionary[Tile, Unit]
+var gridArray
 
 var print_width: int
 
+
 func set_grid(height: int = grid.min_height, width: int = grid.min_width) -> void:
-	gridData.clear()
+	gridArray = create_2d_array(width, height, null)
+	#gridData.clear()
 	#Keep height and width within certain boundary
 	if height < grid.min_height:
 		height = grid.min_height
@@ -27,24 +30,36 @@ func set_grid(height: int = grid.min_height, width: int = grid.min_width) -> voi
 			new_tile.position.x += (w * new_tile.size.x)
 			add_child(new_tile)
 			new_tile.name = "(" + str(w + 1) + ", " + str(h + 1) + ")"
-			gridData[new_tile] = null
-			new_tile.troop_entered.connect(update_griddata)
+			gridArray[h][w] = new_tile
+			#new_tile.troop_entered.connect(update_griddata)
 			#print("Add gridbox at H:",h, "W:", w)
 
 ##This needs to be called EVERY time a unit enters, and exits, a tile.
-func update_griddata(tile_id: Tile, unit_d: Unit) -> void:
-	gridData[tile_id] = unit_d
-	
-	#This is for testing purposes
-	var i: int = 1
-	var j: String = ""
-	print("Grid Data Updated")
-	for tile in gridData:
-		if i > print_width:
-			i = 1
-			j += "\n"
-		
-		i += 1
-		j += str(tile) + " : " + str(gridData[tile]) + " | "
-	
-	print(j)
+#func update_griddata(tile_id: Tile, unit_d: Unit) -> void:
+	#gridData[tile_id] = unit_d
+	#
+	##This is for testing purposes
+	#var i: int = 1
+	#var j: String = ""
+	#print("Grid Data Updated")
+	#for tile in gridData:
+		#if i > print_width:
+			#i = 1
+			#j += "\n"
+		#
+		#i += 1
+		#j += str(tile) + " : " + str(gridData[tile]) + " | "
+	#
+	#print(j)
+
+func create_2d_array(width, height, value):
+	var a = []
+
+	for y in range(height):
+		a.append([])
+		a[y].resize(width)
+
+		for x in range(width):
+			a[y][x] = value
+			
+	return a
